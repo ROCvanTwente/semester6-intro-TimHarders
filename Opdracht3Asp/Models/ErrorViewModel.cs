e291b41e7d937d93b361df3abe488fb5e44d981c
Tim Harders <timwharders@gmail.com>
2025-04-04 13:53:11 +0200

    Opdracht 3 save

---
+++ Opdracht3Asp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Opdracht3Asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
